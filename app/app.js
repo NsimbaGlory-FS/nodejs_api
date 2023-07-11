9f0a6cbbb00a3b6d693759a667bd741a7afeaac6
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-07-11 17:34:51 -0400

    pushed

--- app/app.js
+++ app/app.js
@@ -8,7 +8,8 @@ app.get("/", (req, res, next) => {
   res.status(200).json({ message: "Service is up" });
 });
 //router middleware
-app.use("/image/random", todosRouter);
+app.use("/image/random", router);
+console.log("");
 
 // add middleware to handle error and bad url paths
 app.use((req, res, next) => {
