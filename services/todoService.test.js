ece72b10a1c8f3d342b55e83ee23d95667daff62
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-07-12 10:12:20 -0400

    update

--- services/todoService.test.js
+++ services/todoService.test.js
@@ -6,12 +6,14 @@ describe("To do Service Tests", () => {
     expect(result.data).toHaveLength(200);
     expect(result.data[8].userId).toEqual(1);
     expect(result.data[8].id).toEqual(9);
-    expect(result.data[8].title).toEqual("molestiate perpiciatis ipsa");
+    expect(result.data[8].title).toEqual("molestiae perspiciatis ipsa");
     expect(result.data[8].completed).toEqual(false);
   });
 
   test("As a user I should return a to do object by Id", async () => {
     const result = await todoServiceById(3);
+    console.log("result", result.data);
+
     expect(result.data[0].userId).toEqual(1);
     expect(result.data[0].title).toEqual("fugiat veniam minus");
     expect(result.data[0].completed).toEqual(false);
