8140ebec2aa6230a618daa40ddba3da5f13060d0
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-07-14 09:25:53 -0400

    push

--- app/app.js
+++ app/app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
-  res.status(error, status || 500).json({
+  res.status(error.status || 500).json({
     error: {
       message: error.message,
       status: error.status,
